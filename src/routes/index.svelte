19c1b188071befbef8da6d2ddd71aafa1fe3064b
Garth Braithwaite <garthdb@gmail.com>
2020-09-04 20:58:00 -0600

    added summer blog post

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -36,11 +36,6 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Design Systems Engineer</h1>
 
-<figure>
-	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>A work in progress.</p>
