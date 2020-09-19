c4de7686fbbb3d00703e4c7a93ed211ed876d3a0
garthdb <garthdb@gmail.com>
2020-09-18 21:21:31 -0600

    added week 3 post

---
+++ posts/week-3.md
@@ -0,0 +1,13 @@
+---
+title: Week 3
+slug: week-3
+summary: A week of research.
+date: 2020-09-11
+---
+I have started the Style Dictionary work I mentioned last week with the Hum Drum UI kit tokens.
+
+I've setup my form, but found it wasn't a trivial thing to add it to my Svelte based website.
+
+I also setup appointments with my advisor and another appointment to meet with a few class peers that my professor recommended.
+
+It didn't feel like a very productive week so I'll be putting in more time over the weekend.
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,41 +1,42 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+  h1,
+  figure,
+  p {
+    text-align: center;
+    margin: 0 auto;
+  }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+  h1 {
+    font-size: 2.8em;
+    text-transform: uppercase;
+    font-weight: 700;
+    margin: 0 0 0.5em 0;
+  }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  figure {
+    margin: 0 0 1em 0;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  img {
+    width: 100%;
+    max-width: 400px;
+    margin: 0 0 1em 0;
+  }
 
-	p {
-		margin: 1em auto;
-	}
+  p {
+    margin: 1em auto;
+  }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+  @media (min-width: 480px) {
+    h1 {
+      font-size: 4em;
+    }
+  }
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+  <title>Sapper project template</title>
 </svelte:head>
 
 <h1>Design Systems Engineer</h1>
-
-<p>A work in progress.</p>
+<p>Sign up for my weekly newsletter <a href="https://backyard-studio.ck.page/ad40317bf0">here</a>.</p>
