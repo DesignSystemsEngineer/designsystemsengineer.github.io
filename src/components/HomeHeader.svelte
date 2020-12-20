53a4fc6dc3f701bc69b6571afef2e9bc7947e1d4
Garth Braithwaite <garthdb@gmail.com>
2020-12-19 22:51:53 -0700

    fixed spacing on login link on homepage

--- src/components/HomeHeader.svelte
+++ src/components/HomeHeader.svelte
@@ -3,14 +3,22 @@ import { Button, Grid, GridItem } from "@dsengineer/svelte";
 </script>
 
 <style>
-  :global(.content) {
+  :global(.homeheader) {
     max-width: 768px;
     margin: 0 auto;
+    padding-top: 30px;
+  }
+  :global(.loginButtonContainer) {
+    grid-column-start: 5;
+    grid-column-end: 7;
+    text-align: right;
   }
 </style>
 
-<Grid class="content">
+<Grid class="homeheader">
   <GridItem colSpan="2">
+  </GridItem>
+  <GridItem colSpan="2" class="loginButtonContainer">
     <Button href="courses">email login link</Button>
   </GridItem>
 </Grid>
