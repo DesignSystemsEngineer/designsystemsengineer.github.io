a9ac579165784b339546fc616189d9a4f5292129
Garth Braithwaite <garthdb@gmail.com>
2020-12-20 01:57:55 -0700

    updated home hero

--- src/components/HomeHeader.svelte
+++ src/components/HomeHeader.svelte
@@ -1,24 +1,35 @@
 <script>
-import { Button, Grid, GridItem } from "@dsengineer/svelte";
+import { Button, Grid, GridItem, Heading } from "@dsengineer/svelte";
 </script>
 
 <style>
   :global(.homeheader) {
     max-width: 768px;
     margin: 0 auto;
-    padding-top: 30px;
+    padding: 20px 0 20px 0;
+    align-items: center;
   }
   :global(.loginButtonContainer) {
     grid-column-start: 5;
     grid-column-end: 7;
     text-align: right;
   }
+  .fullwidth {
+    background: white;
+    border-bottom: 1px solid var(--dse-color-gray-200);
+  }
+  :global(.logoTitle) {
+    margin: 0;
+    font-weight: var(--dse-font-weight-semi-bold);
+    text-transform: uppercase;
+  }
 </style>
 
-<Grid class="homeheader">
-  <GridItem colSpan="2">
-  </GridItem>
-  <GridItem colSpan="2" class="loginButtonContainer">
-    <Button href="courses">email login link</Button>
-  </GridItem>
-</Grid>
+<div class="fullwidth">
+  <Grid class="homeheader">
+    <Heading size="XS" class="dse-grid__2col logoTitle">Design Systems Engineer</Heading>
+    <GridItem colSpan="2" class="loginButtonContainer">
+      <Button href="courses">email login link</Button>
+    </GridItem>
+  </Grid>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,13 +10,65 @@
     position: relative;
     z-index: 2;
   }
-  .glacierImg {
+  .hero-container {
     width: 100%;
+    height: 50vh;
+    display: flex;
     position: relative;
-    top: -120px;
-    left: -20px;
-    opacity: 0.2;
-    z-index: 0;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  .hero-container::before {
+    content: "";
+    background: url("/bee.svg");
+    background-size: cover;
+    background-position: center;
+    opacity: 0.5;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+  }
+  .solo-bee {
+    position: absolute;
+    left: 25%;
+    top: 15%;
+  }
+  :global(.hero-container--heading){
+    position: relative;
+    margin: 0;
+    max-width: 500px;
+    text-align: center;
+  }
+  :global(.hero-container--heading.primary) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  :global(.hero-container--heading.secondary) {
+    font-weight: var(--dse-font-weight-semi-bold);
+    text-transform: uppercase;
+    color: var(--dse-color-gray-900);
+    margin-bottom: 5px;
+  }
+  :global(.hero-container--heading.primary span) {
+    background: rgba(255,255,255,0.8);
+    padding: 4px 16px;
+    overflow: hidden;
+    white-space: nowrap;
+    box-sizing: border-box;
+    font-weight: var(--dse-font-weight-bold);
+  }
+  :global(.hero-container--heading.primary span:nth-of-type(1)) {
+    border-radius: 8px 8px 0 0;
+  }
+  :global(.hero-container--heading.primary span:nth-of-type(2)) {
+    border-radius: 8px;
+  }
+  :global(.hero-container--heading.primary span:nth-of-type(3)) {
+    border-radius: 0 0 8px 8px;
   }
 </style>
 
@@ -25,17 +77,12 @@
 </svelte:head>
 
 <Page>
+  <div class="hero-container">
+    <img class="solo-bee" src="solo-bee.svg" width="100" alt="flying bee"/>
+    <Heading size="XS" class="hero-container--heading secondary">New Design System Course</Heading>
+    <Heading size="XL" class="hero-container--heading primary"><span>Establish, Maintain, and Scale</span> <span>a Design System That Your Team</span> <span>Will Want to Use</span></Heading>
+  </div>
   <Grid class="content">
-    <GridItem colSpan="3">
-      <Heading class="mainTitle" size="XXXL">Design Systems Engineer</Heading>
-    </GridItem>
-    <GridItem colSpan="3" />
-    <GridItem colSpan="6">
-      <img
-        class="glacierImg"
-        src="/glacier.jpg"
-        alt="Hidden Lake at Glacier National Park" />
-    </GridItem>
     <GridItem colSpan="6">
       <p>
         The Design Systems Engineering course content is built for engineers to
